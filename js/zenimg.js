406cdba9e1384416d26b663c9d095190fa945a10
Jamie Isaacs <pdt256@gmail.com>
2013-05-19 22:42:46 -0700

    Incrementing release versions.

--- js/zenimg.js
+++ js/zenimg.js
@@ -1,7 +1,7 @@
 /*
 #  Copyright 2012 Ink Labs, LLC
 #
-#  v.1.0.3
+#  v.1.0.4
 # 
 #  Licensed under the Apache License, Version 2.0 (the "License"); you may
 #  not use this file except in compliance with the License. You may obtain
