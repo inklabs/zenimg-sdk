20ec6f271b62a0b1c3f21e5dbb8187d6c297e22d
Jamie Isaacs <jisaacs@cj.com>
2018-06-28 13:31:00 -0700

    Adding test for Image Builder

---
+++ go/pkg/zenimg/image_test.go
@@ -0,0 +1,19 @@
+package zenimg_test
+
+import (
+	"testing"
+
+	"github.com/inklabs/zenimg-sdk/go/pkg/zenimg"
+)
+
+func TestImageBuilder(t *testing.T) {
+	// given
+	imageBuilder := zenimg.NewImageBuilder("http://i.zenimg.com")
+	image := zenimg.NewPoster().Width(200).Height(200).FormatJpg()
+
+	// when
+	url := imageBuilder.GetImageUrl("e9c4937703e644ecb5abb7ceb6233c21", image)
+
+	// then
+	assertEqual(t, "http://i.zenimg.com/e9c4937703e644ecb5abb7ceb6233c21_PO_200X200.jpg", url)
+}
